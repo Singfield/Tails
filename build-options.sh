aa8f46be3b412304d177b22105f9d6612cbda37c
Louis Rigaud <tenno@macbook-pro-de-louis-1.home>
2022-11-20 22:26:46 +0100

    init bash version

---
+++ build-options.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# customize with your own.
+options=("arm64-pc-linux-gnu" "aarch64-apple-darwin" "x86_64-pc-linux-gnu")
+
+menu() {
+    echo "Avaliable options:"
+    for i in ${!options[@]}; do 
+        printf "%3d%s) %s\n" $((i+1)) "${choices[i]:- }" "${options[i]}"
+    done
+    if [[ "$msg" ]]; then echo "$msg"; fi
+}
+
+prompt="Check an option (again to uncheck, ENTER when done): "
+while menu && read -rp "$prompt" num && [[ "$num" ]]; do
+    [[ "$num" != *[![:digit:]]* ]] &&
+    (( num > 0 && num <= ${#options[@]} )) ||
+    { msg="Invalid option: $num"; continue; }
+    ((num--)); msg="${options[num]} was ${choices[num]:+un}checked"
+    [[ "${choices[num]}" ]] && choices[num]="" || choices[num]="+"
+done
+
+printf "You selected"; msg=" nothing"
+for i in ${!options[@]}; do 
+    [[ "${choices[i]}" ]] && { printf " %s" "${options[i]}"; msg=""; export CHOICE=$(echo "${options[i]}");} 
+done
+
+echo "$msg"
---
+++ run.sh
@@ -0,0 +1,102 @@
+#!/bin/bash
+
+directory="src/"
+build_directory="bin"
+log_file="./log.txt"
+counter=0
+compiler="clang"
+
+#colors output
+
+black=$(tput setaf 0)
+red=$(tput setaf 1)
+green=$(tput setaf 2)
+yellow=$(tput setaf 3)
+lime_yellow=$(tput setaf 190)
+powder_blue=$(tput setaf 153)
+blue=$(tput setaf 4)
+magenta=$(tput setaf 5)
+cyan=$(tput setaf 6)
+white=$(tput setaf 7)
+bright=$(tput bold)
+normal=$(tput sgr0)
+
+italic=$(tput sitm)
+
+
+
+echo "                                                                             "
+echo "                                                                             "
+echo "${magenta} ████████╗ ${normal}${cyan} █████╗ ${normal}${powder_blue}██╗${normal} ${green}██╗      ${normal}${red}███████╗${normal}    ██████╗ ██╗   ██╗██╗██╗     ██████╗  " 
+echo "${magenta} ╚══██╔══╝ ${normal}${cyan}██╔══██╗${normal}${powder_blue}██║${normal} ${green}██║      ${normal}${red}██╔════╝${normal}    ██╔══██╗██║   ██║██║██║     ██╔══██╗ "
+echo "${magenta}    ██║    ${normal}${cyan}███████║${normal}${powder_blue}██║${normal} ${green}██║      ${normal}${red}███████╗${normal}    ██████╔╝██║   ██║██║██║     ██║  ██║ "
+echo "${magenta}    ██║    ${normal}${cyan}██╔══██║${normal}${powder_blue}██║${normal} ${green}██║      ${normal}${red}╚════██║${normal}    ██╔══██╗██║   ██║██║██║     ██║  ██║ "
+echo "${magenta}    ██║    ${normal}${cyan}██║  ██║${normal}${powder_blue}██║${normal} ${green}███████╗ ${normal}${red}███████║${normal}    ██████╔╝╚██████╔╝██║███████╗██████╔╝ "
+echo "${magenta}    ╚═╝    ${normal}${cyan}╚═╝  ╚═╝${normal}${powder_blue}╚═╝${normal} ${green}╚══════╝ ${normal}${red}╚══════╝${normal}    ╚═════╝  ╚═════╝ ╚═╝╚══════╝╚═════╝  "
+echo " v0.5  bash ~ horus version.                                                 "
+echo "                                                                             "                                                                            
+echo "${lime_yellow}${italic}  I can fail, like a yellow fox 's gadgets, so improve me.${normal} "
+echo "                                                                             "    
+
+. ./build-options.sh
+
+if [ -z "$(which inotifywait)" ]; then
+    echo "inotifywait not installed."
+    echo " In most distros, it is available in the inotify-tools package. "
+    exit 1
+fi
+
+if [[ -z "${CHOICE}" ]]; then
+  BUILD_OPTION="Some default value because CHOICE is undefined"
+  echo "$BUILD_OPTION"
+exit
+
+else
+  BUILD_OPTION="${CHOICE}"
+fi
+
+
+function build_directory(){
+    if [ -d "$build_directory" ];
+    then
+        for file in $build_directory
+        do
+            rm -rf "$file"
+            mkdir "$build_directory"
+        done
+        
+    else
+        mkdir "$build_directory"
+    fi
+}
+
+
+# Todo create a terminal for selection.
+# clang --print-supported-cpus
+# https://clang.llvm.org/docs/CrossCompilation.html
+function build(){
+    counter=$((counter+1))
+    echo "${yellow} Detected change n. $counter ${normal}"
+    echo "${blue} building... ($compiler) ${normal}"
+    build_directory
+   clang -target $BUILD_OPTION -std=c99 src/*.c -o ./bin/executable
+}
+
+function execute() {
+    sleep .5
+    ./bin/executable && echo
+}
+
+inotifywait -q -r  -m -e modify,delete,create $directory --exclude $build_directory  | while read DIRECTORY EVENT FILE; do
+    case $EVENT in
+        MODIFY*)
+            clear && build && execute
+            ;;
+        CREATE*)
+            clear && build
+            ;;
+        DELETE*)
+            clear && build
+            ;;
+    esac
+done
